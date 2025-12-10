1fa7d662e013f2f15bf2cadb2f2398db64969c52
Jerry <playstore1392004@gmail.com>
2025-12-10 22:28:43 +0530

    Loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 // import gsap from 'gsap';
 // import {useGSAP} from '@gsap/react';
 
+import { useEffect } from "react";
+import About from "./components/about";
 import Hero from "./components/Hero";
 
 
 function App() {
 
+  useEffect(() => {
+    const loader = document.getElementById("loader");
+    if(loader) loader.style.display = "none";
+  },[]);
   return (
     <>
       <Hero />
+      <About />
     </>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,25 +6,40 @@ import { useGSAP } from '@gsap/react';
 gsap.registerPlugin(useGSAP);
 
 function Hero() {
+  
 
-  useGSAP(() => {
-    gsap.fromTo(".profile", 
-      { scale:"5",
-        rotate:"0",
-        top:"50%",left:"50%",translateX:"-50%",translateY:"-50%",zIndex:"2"
-       },   // image spans full width
-      { 
-        scale:"1.3",      
-        duration: 1.8,
-        ease: "power4.inOut",top:"60%",left:"50%",translateX:"-60%",translateY:"-50%",rotate:"-15deg",zIndex:"1",
-      }
-    );
-  });
+  if(window.innerWidth > 1000){
+    useGSAP(() => {
+      gsap.fromTo(".profile", 
+        { scale:"5",
+          rotate:"0",
+          top:"50%",left:"50%",translateX:"-50%",translateY:"-50%",zIndex:"2"
+        },   
+        { 
+          scale:"1.3",      
+          duration: 1.8,
+          ease: "power4.inOut",top:"60%",left:"50%",translateX:"-60%",translateY:"-50%",rotate:"-15deg",zIndex:"1",
+        }
+      );
+    });
+  }else{
+    useGSAP(() => {
+      gsap.fromTo(".profile", 
+        { scale:"9",
+          rotate:"0",
+          top:"50%",left:"50%",translateX:"-50%",translateY:"-50%",zIndex:"2"
+        },   
+        { 
+          scale:"2.5",      
+          duration: 1.8,
+          ease: "power4.inOut",top:"60%",left:"50%",translateX:"-60%",translateY:"-50%",rotate:"-15deg",zIndex:"1",
+        }
+      );
+    });
 
+  }
   return (
     <section className="hero">
-        <span className="grey"></span>
-
       <div className="container">
         <div className="wrapper">
           <span className="name">Deep Mujpara</span>
