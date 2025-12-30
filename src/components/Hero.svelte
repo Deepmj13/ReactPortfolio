a6f7414846f466e7483ecffc6d395613e60dacf4
Jerry <playstore1392004@gmail.com>
2025-12-30 17:18:29 +0530

    Hero

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,3 +1,27 @@
+<script>
+  import { onMount } from "svelte";
+  import profile from "../assets/profile.webp";
+  import gsap from "gsap";
+
+  let image;
+  onMount(() => {
+    gsap.fromTo(
+      image,
+      {
+        scale: 5,
+        opacity: 1,
+      },
+      {
+        scale: 0.2,
+        duration: 1,
+        rotation: "-10deg",
+        ease: "power3.out",
+        opacity: 0.7,
+      }
+    );
+  });
+</script>
+
 <section class="hero">
   <header>
     <span class="portfolio"> Portfolio </span>
@@ -6,6 +30,9 @@
     </span>
   </header>
   <div class="content">
+    <div class="profile">
+      <img bind:this={image} src={profile} alt="" />
+    </div>
     <h1>Deep Mujpara</h1>
     <h4>Software Developer</h4>
   </div>
